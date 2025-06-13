ff097049d6e6f2f0248c306131fde9b9cd43738c
SoleneOclock <solene@oclock.io>
2025-06-13 14:24:18 +0200

    bug fix pour deploiement

--- src/components/MeteoCard.tsx
+++ src/components/MeteoCard.tsx
@@ -7,26 +7,29 @@ export default async function MeteoCard({ city }: { city: string }) {
 	// on peut direct faire le fetch attendre les données et envoyer direct au client une page avec les données
 	// un composant rendu coté serveur peut etre asynchrone ! le client attendra un peu avant d'avoir sa page ...
 
-  // simuler un delais
-  //await new Promise(resolve => setTimeout(resolve, 3000));
+	// simuler un delais
+	//await new Promise(resolve => setTimeout(resolve, 3000));
 
 	const data = await fetch(
 		`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`,
 	);
 	const meteo = await data.json();
 	const temp = meteo.main.temp;
-  const icon = meteo.weather[0].icon;
-  
-  return (
+	const icon = meteo.weather[0].icon;
+
+	return (
 		<Link
 			href={`/city/${city}`}
 			className="bg-blue-200 border-2 p-2 rounded-2xl text-cyan-800 w-1/2 flex border-white"
 		>
-			<img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="icon" />
+			<img
+				src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+				alt="icon"
+			/>
 			<div>
 				<div>{city}</div>
 				<div className="text-2xl">{temp}°C</div>
 			</div>
-		</Link>,
+		</Link>
 	);
 }
